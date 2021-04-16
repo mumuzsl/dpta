75ceec4490949c61a39b892650612543a66795d2
hkisme <1033506716@qq.com>
2021-04-16 18:32:43 +0800

    ...

--- start/src/test/java/com/cqjtu/dpta/DptaApplicationTests.java
+++ start/src/test/java/com/cqjtu/dpta/DptaApplicationTests.java
@@ -1,10 +1,18 @@
 package com.cqjtu.dpta;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.cqjtu.dpta.api.CommRService;
+import com.cqjtu.dpta.dao.entity.CommR;
 import com.cqjtu.dpta.dao.entity.Distr;
+import com.cqjtu.dpta.dao.mapper.CommRMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author mumu
@@ -13,8 +21,20 @@ import javax.annotation.Resource;
 @SpringBootTest
 public class DptaApplicationTests {
 
+    @Resource
+    private CommRMapper commRMapper;
     @Test
     void contextLoads() {
+        Page<CommR> userPage = new Page<>(1, 2);
+        IPage<CommR> iPage = commRMapper.bindSort(userPage);
+        System.out.println("总页数：" + iPage.getPages());
+        System.out.println("总记录数：" + iPage.getTotal());
+        if (iPage.getRecords().size() != 0) {
+            List<CommR> mpUserList1 = iPage.getRecords();
+            mpUserList1.forEach(System.out::println);
+        } else {
+            System.out.println("数据已经加载完成");
+        }
     }
 
 //    @Resource
