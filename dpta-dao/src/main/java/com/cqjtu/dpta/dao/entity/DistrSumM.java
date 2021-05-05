dbbeecf1687e0a29a33c6d47849110d5925b6b2f
hkisme <1033506716@qq.com>
2021-05-05 15:07:23 +0800

    ...

---
+++ dpta-dao/src/main/java/com/cqjtu/dpta/dao/entity/DistrSumM.java
@@ -0,0 +1,17 @@
+package com.cqjtu.dpta.dao.entity;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author ly
+ * @date 2021-05-04 15:06
+ */
+
+@Data
+public class DistrSumM {
+
+    private String distrName;
+    private BigDecimal distrM;
+}
